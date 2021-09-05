1631b693799455a18d99a44c6f2a8e300a34dcfb
Tabish Shaikh <tabish@archive.org>
2021-09-06 00:35:38 +0530

    updated hardhat config to deploy on testnet

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-deploy");
 
+require("dotenv").config();
+
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL ||
+  "https://eth-rinkeby.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "your private key";
+const API_KEY = process.env.API_KEY || "your private key";
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -27,5 +35,15 @@ module.exports = {
     hardhat: {
       chainId: 1337, // specific to how hardhat works
     },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: API_KEY,
   },
 };
