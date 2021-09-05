5c15a92794bc6ca96b997229d4bfe14fddc14e01
Tabish Shaikh <tabish@archive.org>
2021-09-05 16:43:04 +0530

    cleanup: index js seperated from app.js

--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -1,61 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { useWallet, UseWalletProvider } from "use-wallet";
 
+import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-function App() {
-  const wallet = useWallet();
-  const blockNumber = wallet.getBlockNumber();
-
-  return (
-    <>
-      <h1>Wallet</h1>
-      {wallet.status === "connected" ? (
-        <div>
-          <div>Account: {wallet.account}</div>
-          <div>Balance: {wallet.balance}</div>
-          <button onClick={() => wallet.reset()}>disconnect</button>
-        </div>
-      ) : (
-        <div>
-          Connect:
-          <button onClick={() => wallet.connect()}>MetaMask</button>
-          <button onClick={() => wallet.connect("frame")}>Frame</button>
-          <button onClick={() => wallet.connect("portis")}>Portis</button>
-        </div>
-      )}
-    </>
-  );
-}
-
-const Main = () => {
-  const [address, setAddress] = useState("");
-
-  const ethereum = window.ethereum;
-
-  ethereum.on("accountsChanged", function (accounts) {
-    setAddress(accounts[0]);
-  });
-
-  return (
-    <div className="container">
-      {ethereum && <p>Your connected ethereum address: {address}</p>}
-      Hello World{" "}
-    </div>
-  );
-};
-
 ReactDOM.render(
-  <UseWalletProvider
-    chainId={1}
-    connectors={{
-      // This is how connectors get configured
-      portis: { dAppId: "my-dapp-id-123-xyz" },
-    }}
-  >
+  <React.StrictMode>
     <App />
-    <Main></Main>
-  </UseWalletProvider>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
